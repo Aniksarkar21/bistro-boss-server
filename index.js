579fc6b3753e2b4a991cb97a86e111ada3aa8753
Aniksarkar21 <www.anik225@gmail.com>
2025-11-10 17:47:47 +0600

    carts added

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uok3yga.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,6 +31,8 @@ async function run() {
     const menuCollection = client.db("bistroDb").collection("menu");
     const reviewCollection = client.db("bistroDb").collection("reviews");
     
+    const cartCollection = client.db("bistroDb").collection("carts");
+    
     app.get('/menu', async(req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
@@ -39,6 +41,27 @@ async function run() {
        
     app.get('/reviews', async(req, res) => {
       const result = await reviewCollection.find().toArray();
+      res.send(result);
+    });
+    // carts collection 
+    app.get('/carts', async (req, res) => {
+      const email = req.query.email;
+      const query = {email: email};
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+
+        app.post('/carts', async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
+
+    });
+
+    app.delete('/carts/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await cartCollection.deleteOne(query);
       res.send(result);
     })
 
@@ -63,3 +86,17 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Bistro boss is sitting on port ${port}`);
 })
+
+
+/***
+ * --------------------------
+ * NAMING CONVENTION
+ * --------------------------
+ * app.get('/users')
+ * app.get('/users/:id')
+ * app.post('/users')
+ * app.put('/users/:id')
+ * app.patch('users/:id')
+ * app.delete('users/id')
+ * 
+ */
